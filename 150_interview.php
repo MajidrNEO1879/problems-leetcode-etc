88fc1d33c53b02936d2786cfbeec9fd8ddad3a7c
MajidrNEO1879 <majidrezaeiphy1879@gmail.com>
2024-09-01 12:06:43 +0100

    150 q4

--- 150_interview.php
+++ 150_interview.php
@@ -9,25 +9,81 @@ and the last n elements are set to 0 and should be ignored. nums2 has a length o
 
 function merge(array $nums1, int $m, array $nums2, int $n)
 {
-    $newNum1=[];
-    for($i=0;$i<$m;$i++)
-    {
-        $newNum1[]=$nums1[$i];
+    $newNum1 = [];
+    for ($i = 0; $i < $m; $i++) {
+        $newNum1[] = $nums1[$i];
     }
-    for ($j=0;$j<$n;$j++)
-    {
-        $newNum1[]=$nums2[$j];
+    for ($j = 0; $j < $n; $j++) {
+        $newNum1[] = $nums2[$j];
     }
     sort($newNum1);
     return $newNum1;
-}       
+}
 
-var_dump(merge([1,2,3,0,0,0],3,[2,5,6],3));
-var_dump(merge([0],0,[1],1));
+// var_dump(merge([1,2,3,0,0,0],3,[2,5,6],3));
+// var_dump(merge([0],0,[1],1));
 
-//iven an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be
+//given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be
 // changed. Then return the number of elements in nums which are not equal to val.
-// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are
-// not important as well as the size of nums.
-// Return k.
+
+
+function removeEL(array $nums, int $value)
+{
+
+    foreach ($nums as $key => $num) {
+        if ($num == $value) {
+            unset($nums[$key]);
+        }
+    }
+    //or
+    // return array_values(array_filter($nums, function($num) use ($value) {
+    //     return $num != $value;
+    // }));
+    array_values($nums);
+    return count($nums);
+}
+// var_dump(removeEL([0,1,2,2,3,0,4,2],2));
+
+/**Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element 
+ * appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums. */
+
+function removeDup(array $nums)
+{
+
+    $uniqueIndex = 0;
+
+    for ($i = 1; $i < count($nums); $i++) {
+        if ($nums[$i] !== $nums[$uniqueIndex]) {
+            $uniqueIndex++;
+            $nums[$uniqueIndex] = $nums[$i];
+        }
+    }
+    return $uniqueIndex + 1;
+}
+
+// var_dump(removeDup([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
+/**Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice.
+ *  The relative order of the elements should be kept the same. Return k after placing the final result in the first k slots of nums.
+ Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.*/
+
+function remDuplicate2(array &$nums)
+{
+    $n = count($nums);
+    if ($n <= 2) {
+        return $n;
+    }
+    $index = 2;
+    for ($i = 2; $i < $n; $i++) {
+        if ($nums[$i] !== $nums[$index - 2]) {
+            $nums[$index] = $nums[$i];
+            $index++;
+        }
+    }
+
+    return $index;
+}
+$nums = [1, 1, 1, 2, 2, 3];
+$k = remDuplicate2($nums);
+var_dump($k);
+var_dump(array_slice($nums, 0, $k));
